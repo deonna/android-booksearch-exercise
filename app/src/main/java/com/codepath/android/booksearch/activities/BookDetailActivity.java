0da5b082837042c6db7d06e4efa1d59e7eeaa677
Deonna Hodges <deonna@learnersguild.org>
2017-03-15 21:04:41 -0700

    feat(bookSearch): Completed version.

--- app/src/main/java/com/codepath/android/booksearch/activities/BookDetailActivity.java
+++ app/src/main/java/com/codepath/android/booksearch/activities/BookDetailActivity.java
@@ -1,38 +1,132 @@
 package com.codepath.android.booksearch.activities;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.ShareActionProvider;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.codepath.android.booksearch.R;
+import com.codepath.android.booksearch.models.Book;
+import com.squareup.picasso.Callback;
+import com.squareup.picasso.Picasso;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class BookDetailActivity extends AppCompatActivity {
-    private ImageView ivBookCover;
-    private TextView tvTitle;
-    private TextView tvAuthor;
+
+    @BindView(R.id.ivBookCover) ImageView ivBookCover;
+    @BindView(R.id.tvTitle) TextView tvTitle;
+    @BindView(R.id.tvAuthor) TextView tvAuthor;
+    @BindView(R.id.tbBooks) Toolbar tbBooks;
+
+    private ShareActionProvider miShareAction;
+    private Intent shareIntent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_book_detail);
-        // Fetch views
-        ivBookCover = (ImageView) findViewById(R.id.ivBookCover);
-        tvTitle = (TextView) findViewById(R.id.tvTitle);
-        tvAuthor = (TextView) findViewById(R.id.tvAuthor);
 
+        ButterKnife.bind(this);
         // Extract book object from intent extras
 
+        setSupportActionBar(tbBooks);
+
+        Book book = getIntent().getParcelableExtra(BookListActivity.KEY_BOOK);
+
+        Picasso
+                .with(this)
+                .load(book.getCoverUrl())
+                .into(ivBookCover, new Callback() {
+                    @Override
+                    public void onSuccess() {
+                        prepareShareIntent();
+                        attachShareIntentAction();
+                    }
+
+                    @Override
+                    public void onError() {
+
+                    }
+                });
+
+        tvTitle.setText(book.getTitle());
+        tvAuthor.setText(book.getAuthor());
+
+        getSupportActionBar().setTitle(book.getTitle());
         // Use book object to populate data into views
     }
 
+    private void attachShareIntentAction() {
+
+        if (miShareAction != null & shareIntent !=null) {
+            miShareAction.setShareIntent(shareIntent);
+        }
+    }
+
+    private void prepareShareIntent() {
+
+        Uri bmpUri = getLocalBitmapUri(ivBookCover);
+
+        shareIntent = new Intent();
+        shareIntent.setAction(Intent.ACTION_SEND);
+        shareIntent.putExtra(Intent.EXTRA_STREAM, bmpUri);
+        shareIntent.setType("image/*");
+    }
+
+    public Uri getLocalBitmapUri(ImageView imageView) {
+        // Extract Bitmap from ImageView drawable
+        Drawable drawable = imageView.getDrawable();
+        Bitmap bmp = null;
+        if (drawable instanceof BitmapDrawable){
+            bmp = ((BitmapDrawable) imageView.getDrawable()).getBitmap();
+        } else {
+            return null;
+        }
+        // Store image to default external storage directory
+        Uri bmpUri = null;
+        try {
+            // Use methods on Context to access package-specific directories on external storage.
+            // This way, you don't need to request external read/write permission.
+            // See https://youtu.be/5xVh-7ywKpE?t=25m25s
+            File file =  new File(getExternalFilesDir(Environment.DIRECTORY_PICTURES), "share_image_" + System.currentTimeMillis() + ".png");
+            FileOutputStream out = new FileOutputStream(file);
+            bmp.compress(Bitmap.CompressFormat.PNG, 90, out);
+            out.close();
+            // **Warning:** This will fail for API >= 24, use a FileProvider as shown below instead.
+            bmpUri = Uri.fromFile(file);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return bmpUri;
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_book_detail, menu);
+
+        MenuItem item = menu.findItem(R.id.action_share);
+
+        miShareAction = (ShareActionProvider) MenuItemCompat.getActionProvider(item);
+        attachShareIntentAction();
+
         return true;
     }
 
@@ -44,7 +138,7 @@ public class BookDetailActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_share) {
             return true;
         }
 
--- app/src/main/java/com/codepath/android/booksearch/activities/BookListActivity.java
+++ app/src/main/java/com/codepath/android/booksearch/activities/BookListActivity.java
@@ -1,10 +1,17 @@
 package com.codepath.android.booksearch.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.SearchView;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.ProgressBar;
 
 import com.codepath.android.booksearch.R;
 import com.codepath.android.booksearch.adapters.BookAdapter;
@@ -18,26 +25,52 @@ import org.json.JSONObject;
 
 import java.util.ArrayList;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnItemClick;
 import cz.msebera.android.httpclient.Header;
 
 
 public class BookListActivity extends AppCompatActivity {
-    private ListView lvBooks;
-    private BookAdapter bookAdapter;
+
+    public static final String KEY_BOOK = "book";
+
+    @BindView(R.id.lvBooks) ListView lvBooks;
+    @BindView(R.id.tbBooks) Toolbar tbBooks;
+
+    private MenuItem miActionProgressItem;
+
     private BookClient client;
+    private BookAdapter bookAdapter;
+
+    private ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_book_list);
-        lvBooks = (ListView) findViewById(R.id.lvBooks);
+        ButterKnife.bind(this);
+
+        setSupportActionBar(tbBooks);
+
         ArrayList<Book> aBooks = new ArrayList<>();
         // initialize the adapter
         bookAdapter = new BookAdapter(this, aBooks);
         // attach the adapter to the ListView
         lvBooks.setAdapter(bookAdapter);
         // Fetch the data remotely
-        fetchBooks("Oscar Wilde");
+        fetchBooks("");
+
+    }
+
+    @OnItemClick(R.id.lvBooks)
+    public void openDetailView(AdapterView<?> parent, View view, int position, long id) {
+
+        Book book = bookAdapter.getItem(position);
+
+        Intent intent = new Intent(BookListActivity.this, BookDetailActivity.class);
+        intent.putExtra(KEY_BOOK, book);
+        startActivity(intent);
     }
 
     // Executes an API call to the OpenLibrary search endpoint, parses the results
@@ -61,6 +94,7 @@ public class BookListActivity extends AppCompatActivity {
                             bookAdapter.add(book); // add book through the adapter
                         }
                         bookAdapter.notifyDataSetChanged();
+                        hideProgressBar();
                     }
                 } catch (JSONException e) {
                     // Invalid JSON format, show appropriate error.
@@ -79,7 +113,47 @@ public class BookListActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_book_list, menu);
+        MenuItem searchItem = menu.findItem(R.id.action_search);
+        final SearchView searchView = (SearchView) MenuItemCompat.getActionView(searchItem);
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                fetchBooks(query);
+                searchView.clearFocus();
+
+                showProgressBar();
+
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
         return true;
+    }
+
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+
+        miActionProgressItem = menu.findItem(R.id.miActionProgress);
+
+        ProgressBar progressBar = (ProgressBar) MenuItemCompat.getActionView(miActionProgressItem);
+
+        return super.onPrepareOptionsMenu(menu);
+    }
+
+
+    public void showProgressBar() {
+
+        miActionProgressItem.setVisible(true);
+    }
+
+    public void hideProgressBar() {
+
+        miActionProgressItem.setVisible(false);
     }
 
     @Override
@@ -90,7 +164,7 @@ public class BookListActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_share) {
             return true;
         }
 
